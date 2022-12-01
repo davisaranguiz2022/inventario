adcf20562be064574eab6c0e5178925c7fc4f8d7
davis <da.aranguiz@duocuc.cl>
2022-11-30 22:34:11 -0300

    carga inicial

--- e2e/src/tabs.e2e-spec.ts
+++ e2e/src/tabs.e2e-spec.ts
@@ -1,18 +1,26 @@
 import { browser, element, by} from 'protractor';
 
 describe('Mi prueba',()=>{
-    // codigo de configuración
-    beforeEach(()=>{
-        browser.get("/");
-    });
-    // Prueba 1
-    it("El tab 1 se muestra por defecto",()=>{
-        expect(element(by.css(".tab-selected ion-label")).getText()).toContain("Tab 1");
-    });
-    // Prueba 2
-    it("El usuario puede navegar a la pestaña Tab 2",async ()=>{
-        await element(by.css("[tab=tab2]")).click();
-        browser.driver.sleep(500);
-        expect(element(by.css(".tab-selected ion-label")).getText()).toContain("Tab 2");
-    });
+   // Prueba 1
+   it("El tab 1 se muestra por defecto",()=>{
+    expect(element(by.css(".tab-selected ion-label")).getText()).toContain("Tab 1");
+});
+// Prueba 2
+it("El usuario puede navegar a la pestaña Tab 2",async ()=>{
+    await element(by.css("[tab=tab2]")).click();
+    browser.driver.sleep(500);
+    expect(element(by.css(".tab-selected ion-label")).getText()).toContain("Tab 2");
+});
+// Prueba 3
+it("El usuario puede navegar a la pestaña Tab 3",async ()=>{
+    await element(by.css("[tab=tab3]")).click();
+    browser.driver.sleep(500);
+    expect(element(by.css(".tab-selected ion-label")).getText()).toContain("Tab 3");
+});
+ // Prueba 4
+ it("Error, no existe!!",async ()=>{
+    await element(by.css("[tab=tab2]")).click();
+    browser.driver.sleep(500);
+    expect(element(by.css(".tab-selected ion-label")).getText()).toContain("Tab 4");
+    });
 });
